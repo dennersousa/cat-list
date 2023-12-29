452411eef3d45b59dab9d4ab66309cfdd2aa8f86
marcuscarvalhodev <135276762+marcuscarvalhodev@users.noreply.github.com>
2023-12-29 00:55:31 -0300

    to-do list
    criei uma aplicação to-do list com meus gostos

---
+++ app/script.js
@@ -0,0 +1,104 @@
+let todoList = [];
+let itemCounter = 1;
+
+function addItem() {
+    const listType = document.getElementById('listType').value;
+    const itemName = document.getElementById('itemName').value;
+    const quantity = document.getElementById('quantity').value;
+
+    if (itemName.trim() === "") {
+        alert("Por favor, digite um item.");
+        return;
+    }
+
+    const item = { itemName, quantity, completed: false };
+
+    todoList.push(item);
+
+    displayList();
+    adjustContainerHeight();
+
+    // Limpar os campos após adicionar o item
+    document.getElementById('itemName').value = "";
+    document.getElementById('quantity').value = "";
+}
+
+function displayList() {
+    const listContainer = document.getElementById('todoList');
+    listContainer.innerHTML = "";
+
+    todoList.forEach((item, index) => {
+        const listItem = document.createElement('div');
+        listItem.classList.add('list-item');
+
+        const itemName = document.createElement('span');
+        itemName.textContent = `${itemCounter}. ${item.itemName}`;
+        itemName.style.textDecoration = item.completed ? 'line-through' : 'none';
+        listItem.appendChild(itemName);
+
+        if (item.quantity) {
+            const quantity = document.createElement('span');
+            quantity.textContent = `Qtd: ${item.quantity}`;
+            listItem.appendChild(quantity);
+        }
+
+        const completeBtn = document.createElement('button');
+        completeBtn.textContent = item.completed ? 'Desfazer' : 'Concluir';
+        completeBtn.onclick = () => toggleComplete(index);
+        listItem.appendChild(completeBtn);
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'Excluir';
+        deleteBtn.onclick = () => deleteItem(index);
+        listItem.appendChild(deleteBtn);
+
+        listContainer.appendChild(listItem);
+
+        itemCounter++;
+    });
+
+    itemCounter = 1; // Reinicia o contador após a exibição da lista
+}
+
+function toggleComplete(index) {
+    todoList[index].completed = !todoList[index].completed;
+    displayList();
+}
+
+function deleteItem(index) {
+    todoList.splice(index, 1);
+    displayList();
+}
+
+function adjustContainerHeight() {
+    const container = document.getElementById('blur-background');
+    const listItemHeight = 70; // Altura estimada de um item na lista
+    const additionalHeight = 250; // Altura adicional desejada para cada novo item
+
+    // Calcula a altura do contêiner com base na quantidade de itens na lista
+    const newHeight = Math.max(todoList.length * listItemHeight + additionalHeight, 100);
+
+    
+    container.style.height = `${newHeight}px`;
+}
+
+document.getElementById('listType').addEventListener('change', function() {
+    const quantitySection = document.getElementById('quantitySection');
+    if (this.value === 'shopping') {
+        quantitySection.style.display = 'block';
+    } else {
+        quantitySection.style.display = 'none';
+    }
+});
+
+document.getElementById('quantity').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        addItem();
+    }
+});
+
+document.getElementById('itemName').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        addItem();
+    }
+});
